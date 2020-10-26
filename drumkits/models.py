5e3b177e780265652acc5a80dd25980ecaf8aa56
terribledevice <longlost@gmail.com>
2020-10-25 21:14:33 -0400

    getting drum rest setup

--- drumkits/models.py
+++ drumkits/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Drumkits(models.Model):
+    sound = models.CharField(max_length=250)
+    url = models.URLField(max_length=200)
+    library = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.sound
